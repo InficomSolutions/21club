57ad5ca2281324dbeb01c9f9fdc9b8b1c2780f28
Kiraninficom <101560566+Kiraninficom@users.noreply.github.com>
2024-11-22 14:19:43 +0530

    Update connectDB.js

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise";
 
 const connection = mysql.createPool({
-  host: 'database-2.c1yu4u8uoxrf.ap-southeast-2.rds.amazonaws.com',
+  host: 'localhost',
   user: 'admin',
   password: '8600988002',
   database: 'real91',
